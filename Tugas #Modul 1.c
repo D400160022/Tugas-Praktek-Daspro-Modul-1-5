d0d2d8d194477575bcd7a89b341f0960944ca9cf
D400160022 <nurfandi.sabarudin98nufas@gmail.com>
2016-12-22 14:07:31 +0700

    Create Tugas #Modul 1.c

---
+++ Tugas #Modul 1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int p,l;
+    int L;
+
+    printf("Program luas Persegi panjang\n");
+    printf("Masukan nilai p:\n");
+    scanf("%d",&p);
+    printf("Masukan nilai l:\n");
+    scanf("%d",&l);
+    L=p*l;
+    printf("Luas Persegi Panjang = %d",L);
+    return 0;
+
+}
